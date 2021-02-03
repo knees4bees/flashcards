1988b95ed43ba0998030f1a0f5ed82f55057eb99
Katie Ballentine <knees4bees@gmail.com>
2021-02-03 14:02:41 -0700

    Refactor Turn-test to DRY it using hook

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,46 +5,47 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Turn', function() {
+  let card, turn;
 
+  beforeEach(function() {
+    card = new Card(3, 'What\'s the best bird band?', ['The Byrds', 'Black Crowes', 'Flock of Seagulls'], 'Flock of Seagulls');
+    turn = new Turn('The Byrds', card);
+  });
+
+  // TODO ask whether this kind of test is needed
   it('should be a function', function() {
-    const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
+  // TODO ask whether this kind of test is needed
   it('should be an instance of Turn', function() {
-    const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   }); 
 
   it('should store a user\'s guess', function() {
-    const card = new Card(3, 'What\'s the best bird band?', ['The Byrds', 'Black Crowes', 'Flock of Seagulls'], 'Flock of Seagulls');
-    const turn = new Turn('The Byrds', card);
     expect(turn.guess).to.equal('The Byrds');
   });  
 
   it('should store the current card in play', function() {
-    const card = new Card(4, 'How far can a dog run into the woods?', ['Partway', 'Halfway', 'All the way'], 'Halfway');
-    const turn = new Turn('Partway', card);
     expect(turn.currentCard).to.deep.equal(card);
   });  
 });
 
-describe('returnGuess', function() {
-  it('should return the user\'s guess', function() {
-    const card = new Card(5, 'What is the best ice cream?', ['Vanilla', 'React', 'Angular'], 'Vanilla');
-    const turn = new Turn('React', card);
+describe('return user input', function() {
+  let card, turn;
 
+  beforeEach(function() {
+    card = new Card(5, 'What is the best ice cream?', ['Vanilla', 'React', 'Angular'], 'Vanilla');
+    turn = new Turn('React', card);
+  });
+
+  it('should return the user\'s guess', function() {
     const returnedGuess = turn.returnGuess(); 
 
     expect(returnedGuess).to.equal('React');
   });
-});
 
-describe('returnCard', function() {
   it('should return the current card', function() {
-    const card = new Card(5, 'What is the best ice cream?', ['Vanilla', 'React', 'Angular'], 'Vanilla');
-    const turn = new Turn('React', card);
-
     const returnedCard = turn.returnCard(); 
 
     expect(returnedCard).to.deep.equal(card);
@@ -52,8 +53,13 @@ describe('returnCard', function() {
 });
 
 describe('evaluateGuess', function() {
+  let card;
+
+  beforeEach(function() {
+    card = new Card(6, 'Which bear is the best bear?', ['Black', 'Brown', 'Polar'], 'Black');
+  });
+
   it('should indicate that a right answer is correct', function() {
-    const card = new Card(6, 'Which bear is the best bear?', ['Black', 'Brown', 'Polar'], 'Black');
     const turn = new Turn('Black', card);
 
     const result = turn.evaluateGuess();
@@ -62,7 +68,6 @@ describe('evaluateGuess', function() {
   });
 
   it('should indicate that a wrong answer is incorrect', function() {
-    const card = new Card(6, 'Which bear is the best bear?', ['Black', 'Brown', 'Polar'], 'Black');
     const turn = new Turn('Brown', card);
 
     const result = turn.evaluateGuess();
@@ -72,8 +77,13 @@ describe('evaluateGuess', function() {
 });
 
 describe('giveFeedback', function() {
+  let card;
+
+  beforeEach(function() {
+    card = new Card(6, 'Which bear is the best bear?', ['Black', 'Brown', 'Polar'], 'Black');
+  });
+
   it('should display an appropriate message when an answer is correct', function() {
-    const card = new Card(6, 'Which bear is the best bear?', ['Black', 'Brown', 'Polar'], 'Black');
     const turn = new Turn('Black', card);
 
     const message = turn.giveFeedback();
@@ -82,7 +92,6 @@ describe('giveFeedback', function() {
   });
 
   it('should display an appropriate message when an answer is incorrect', function() {
-    const card = new Card(6, 'Which bear is the best bear?', ['Black', 'Brown', 'Polar'], 'Black');
     const turn = new Turn('Brown', card);
 
     const message = turn.giveFeedback();
