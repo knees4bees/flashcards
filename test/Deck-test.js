b105decb17c6f238f56cc688d3cae3a34f1eaea6
Katie Ballentine <knees4bees@gmail.com>
2021-02-02 22:25:02 -0700

    Create test and implementation for cards property

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const Card = require('../src/Card');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
@@ -14,5 +15,14 @@ describe('Deck', function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   }); 
+
+  it('should begin with an array of cards', function() {
+    const card1 = new Card(1, 'What rhymes with orange?', ['apple', 'pear', 'nothing'], 'nothing');
+    const card2 = new Card(2, 'What rhymes with bear?', ['apple', 'share', 'dear'], 'share');
+    const card3 = new Card(3, 'What rhymes with beer?', ['dear', 'pear', 'been'], 'dear');
+    const deck = new Deck([card1, card2, card3]);
+
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+  });
 
 });
