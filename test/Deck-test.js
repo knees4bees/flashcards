3de4dee8c82672101ae5cd127cf9021e0bbe561a
Katie Ballentine <knees4bees@gmail.com>
2021-02-02 22:29:55 -0700

    Create test and implementation for countCards method

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -25,4 +25,14 @@ describe('Deck', function() {
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
+  it('should know how many cards are in the deck', function() {
+    const card1 = new Card(1, 'What rhymes with orange?', ['apple', 'pear', 'nothing'], 'nothing');
+    const card2 = new Card(2, 'What rhymes with bear?', ['apple', 'share', 'dear'], 'share');
+    const card3 = new Card(3, 'What rhymes with beer?', ['dear', 'pear', 'been'], 'dear');
+    const deck = new Deck([card1, card2, card3]);
+
+    const size = deck.countCards();
+
+    expect(size).to.equal(3);
+  });
 });
