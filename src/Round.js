91b3c0e6d10183b49cf570d31a345dc6769ca4af
Katie Ballentine <knees4bees@gmail.com>
2021-02-03 15:21:30 -0700

    Add and test returnCurrentCard method

--- src/Round.js
+++ src/Round.js
@@ -2,6 +2,10 @@ class Round {
   constructor(deck) {
     this.deck = deck.cards;
   }
+
+  returnCurrentCard() {
+    return this.deck.shift();
+  }
 }
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -6,18 +6,23 @@ const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 describe('Round', function() {
-  let card1, card2, card3, deck;
+  let card1, card2, card3, deck, round;
 
   beforeEach(function() {
     card1 = new Card(1, 'Which snack food is best?', ['chips', 'pretzels', 'cheese'], 'cheese');
     card2 = new Card(2, 'Which lunch food is best?', ['apple', 'sandwich', 'burrito'], 'apple');
     card3 = new Card(3, 'Which drink is best?', ['water', 'coffee', 'tea'], 'tea');
     deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
   });
 
   it('should have a deck', function() {
-    const round = new Round(deck);
-
     expect(round.deck).to.deep.equal([card1, card2, card3]);
+  });
+
+  it('should return the current card being played', function() {
+    const currentCard = round.returnCurrentCard();
+
+    expect(currentCard).to.equal(card1);
   });
 });
